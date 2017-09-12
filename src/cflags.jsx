185e3b6c417cfdf6a026cf57385b4daf1c4cb2ca
IsaacMother <andrey.turbov@gmail.com>
2017-09-12 12:12:00 +0300

    Got somewhat working prototype

--- src/cflags.jsx
+++ src/cflags.jsx
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import './cflags.css';
 
-const Header = ({}) =>
+
+const Header = () =>
   <div>
     <h1>CFlags</h1>
   </div>
 
-const Search = ({value, onSubmit}) =>
+const Search = ({value, onSubmit, onChange}) =>
   <form>
     <input
       type="text"
       value={value}
+      onChange={onChange}
     />
-    <button type="Submit">Submit</button>
+    <button
+      type="Submit"
+      onClick={onSubmit}
+    >Submit</button>
   </form>
 
 // List of flags with description of each
 // In case of invalid flag output an error
-const Description = ({ searchTerm, flagList }) =>
+const Description = ({ searchResult }) =>
   <ul>
-    {  }
+    {searchResult.map(flag =>
+      <li>{flag.id} - {flag.description}</li>
+    )}
   </ul>
 
 class CFlags extends Component {
@@ -27,15 +34,26 @@ class CFlags extends Component {
     super(props);
 
     this.state = {
+      flagList: this.props.flags,
       result: null,
-      searchTerm: null
+      searchTerm: ""
     }
 
     this.onSubmit = this.onSubmit.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  onSearchChange(event) {
+      this.setState({ searchTerm: event.target.value });
   }
 
   onSubmit(event) {
-    this.setState({ searchTerm: event.target.value });
+    let { searchTerm, flagList } = this.state;
+    searchTerm = searchTerm.split(' ');
+    // Search process
+    const searchResult = flagList.filter(item => searchTerm.includes(item.id));
+    this.setState({ result: searchResult });
+    event.preventDefault();
   }
 
   render() {
@@ -43,11 +61,19 @@ class CFlags extends Component {
     return (
       <div className="cflags">
         <Header />
+
         <Search
           value={searchTerm}
           onSubmit={this.onSubmit}
+          onChange={this.onSearchChange}
         />
-        <Description />
+
+        { result
+          ? <Description
+            searchResult={result}
+          />
+          : <p>No such flags found</p>
+        }
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 import CFlags from './cflags';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<CFlags />, document.getElementById('root'));
+const FLAGS = [{ "mmx": "Multimedia Extensions" },
+               { "fdiv": "FPU FDIV" },
+               { "coma": "Cyrix 6x86 coma" },
+               { "msr": "Model-Specific Registers" }
+];
+
+ReactDOM.render(<CFlags flags={FLAGS} />, document.getElementById('root'));
 registerServiceWorker();
 
 if (module.hot) {
